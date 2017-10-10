ef4d666e7dbae7ff764cee39d99d4b0e6b423bd6
Stephen Whitmore <sww@eight45.net>
2017-10-10 16:17:17 -0700

    test: more thorough way/relation/changeset testing

--- test/create.js
+++ test/create.js
@@ -115,62 +115,236 @@ test('create bad nodes', function (t) {
   })
 })
 
-test('create way', function (t) {
-  t.plan(1)
-
-  var db = hyper(ram, { valueEncoding: 'json' })
-  var osm = Osm(db)
-
-  var way = {
-    type: 'way',
-    changeset: '19',
-    refs: ['bob', 'dole', 'for', 'prez']
-  }
-
-  osm.create(way, function (err) {
-    t.error(err)
-  })
-})
-
-test('create bad way', function (t) {
-  t.plan(1)
-
-  var db = hyper(ram, { valueEncoding: 'json' })
-  var osm = Osm(db)
-
-  var way = {
-    type: 'way',
-    changeset: '19',
-    refs: ['bob']
-  }
-
-  osm.create(way, function (err) {
-    t.ok(err instanceof Error)
-  })
-})
-
-test('create relation', function (t) {
-  t.plan(1)
-
-  var db = hyper(ram, { valueEncoding: 'json' })
-  var osm = Osm(db)
-
-  var relation = {
-    type: 'relation',
-    changeset: '19',
-    tags: {
-      waterway: 'river'
+test('create good ways', function (t) {
+  var ways = [
+    {
+      type: 'way',
+      changeset: '19',
+      refs: ['bob', 'dole', 'for', 'prez'],
+      timestamp: '2017-10-10T19:55:08.570Z'
     },
-    members: [
-      {
-        type: 'node',
-        id: '101'
-      }
-    ]
-  }
+    {
+      type: 'way',
+      changeset: '19',
+      refs: ['bob', 'dole', 'for', 'prez']
+    },
+    {
+      type: 'node',
+      changeset: '9',
+      lat: '-11',
+      lon: '-10',
+      extra: 'field'
+    }
+  ]
 
-  osm.create(relation, function (err) {
-    t.error(err)
+  t.plan(ways.length)
+
+  var db = hyper(ram, { valueEncoding: 'json' })
+  var osm = Osm(db)
+
+  ways.forEach(function (node) {
+    osm.create(node, function (err) {
+      t.error(err)
+    })
+  })
+})
+
+test('create bad ways', function (t) {
+  var ways = [
+    {
+      type: 'way'
+    },
+    {
+      type: 'way',
+      changeset: '14'
+    },
+    {
+      type: 'way',
+      changeset: '14',
+      refs: []
+    },
+    {
+      type: 'way',
+      changeset: '14',
+      refs: ['hi']
+    },
+    {
+      type: 'way',
+      changeset: '14',
+      refs: ['hi', 'there']
+    },
+    {
+      type: 'way',
+      changeset: 14,
+      refs: ['hi', 'there', 'friend']
+    },
+    {
+      type: 'way',
+      changeset: '14',
+      refs: ['hi', 'there', 'friend'],
+      timestamp: 'a while ago'
+    },
+  ]
+
+  t.plan(ways.length)
+
+  var db = hyper(ram, { valueEncoding: 'json' })
+  var osm = Osm(db)
+
+  ways.forEach(function (node, idx) {
+    osm.create(node, function (err) {
+      t.ok(err instanceof Error, 'ways['+idx+']')
+    })
+  })
+})
+
+test('create good relations', function (t) {
+  var relations = [
+    {
+      type: 'relation',
+      changeset: '19',
+      tags: { waterway: 'river' },
+      members: [
+        {
+          type: 'node',
+          id: '101'
+        }
+      ]
+    },
+    {
+      type: 'relation',
+      changeset: '19',
+      tags: { waterway: 'river' },
+      members: [
+        {
+          type: 'node',
+          id: '101',
+          role: 'best-friend'
+        }
+      ]
+    },
+    {
+      type: 'relation',
+      changeset: '21',
+      tags: { foo: 'bar' },
+      members: []
+    }
+  ]
+
+  t.plan(relations.length)
+
+  var db = hyper(ram, { valueEncoding: 'json' })
+  var osm = Osm(db)
+
+  relations.forEach(function (node) {
+    osm.create(node, function (err) {
+      t.error(err)
+    })
+  })
+})
+
+test('create bad relations', function (t) {
+  var relations = [
+    {
+      type: 'relation'
+    },
+    {
+      type: 'relation',
+      changeset: '21'
+    },
+    {
+      type: 'relation',
+      changeset: '21',
+      tags: { foo: 'bar' }
+    },
+    {
+      type: 'relation',
+      changeset: '21',
+      tags: {},
+      members: []
+    },
+    {
+      type: 'relation',
+      changeset: '21',
+      tags: { foo: 'bar' },
+      members: {}
+    },
+    {
+      type: 'relation',
+      changeset: '21',
+      tags: { foo: 'bar' },
+      members: [
+        {
+          type: 'sandwich',
+          ref: '17'
+        }
+      ]
+    },
+    {
+      type: 'relation',
+      changeset: '21',
+      tags: { foo: 'bar' },
+      members: [
+        {
+          type: 'way',
+          id: 17
+        }
+      ]
+    },
+  ]
+
+  t.plan(relations.length)
+
+  var db = hyper(ram, { valueEncoding: 'json' })
+  var osm = Osm(db)
+
+  relations.forEach(function (node, idx) {
+    osm.create(node, function (err) {
+      t.ok(err instanceof Error, 'relations['+idx+']')
+    })
+  })
+})
+
+test('create good changesets', function (t) {
+  var changesets = [
+    {
+      type: 'changeset',
+    },
+    {
+      type: 'changeset',
+      timestamp: '2017-10-10T19:55:08.570Z'
+    },
+  ]
+
+  t.plan(changesets.length)
+
+  var db = hyper(ram, { valueEncoding: 'json' })
+  var osm = Osm(db)
+
+  changesets.forEach(function (node) {
+    osm.create(node, function (err) {
+      t.error(err)
+    })
+  })
+})
+
+test('create bad changesets', function (t) {
+  var changesets = [
+    {
+      type: 'changeset',
+      timestamp: 'now'
+    },
+  ]
+
+  t.plan(changesets.length)
+
+  var db = hyper(ram, { valueEncoding: 'json' })
+  var osm = Osm(db)
+
+  changesets.forEach(function (node, idx) {
+    osm.create(node, function (err) {
+      t.ok(err instanceof Error, 'changesets['+idx+']')
+    })
   })
 })
 
