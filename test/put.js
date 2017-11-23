3d740ce955fdf86b1bdec5a30a80b38789c6bf8a
Stephen Whitmore <sww@eight45.net>
2017-11-23 11:05:12 -0800

    test: check deletions

--- test/put.js
+++ test/put.js
@@ -151,3 +151,40 @@ test('update bad nodes', function (t) {
     })
   })
 })
+
+test('delete a node', function (t) {
+  t.plan(5)
+
+  var db = P2P(hyper(ram, { valueEncoding: 'json' }))
+  db.install('osm', Osm(db))
+
+  var node = {
+    type: 'node',
+    changeset: '9',
+    lat: '-11',
+    lon: '-10',
+    timestamp: '2017-10-10T19:55:08.570Z'
+  }
+  var nodeDeletion = {
+    type: 'node',
+    changeset: '10',
+    lat: '-11',
+    lon: '-10',
+    visible: false
+  }
+
+  db.osm.create(node, function (err, id) {
+    t.error(err)
+    db.osm.put(id, nodeDeletion, function (err) {
+      t.error(err)
+      db.osm.get(id, function (err, elms) {
+        t.error(err)
+        t.equals(elms.length, 1)
+        delete elms[0].id
+        delete elms[0].version
+        t.deepEquals(elms[0], nodeDeletion)
+      })
+    })
+  })
+})
+
