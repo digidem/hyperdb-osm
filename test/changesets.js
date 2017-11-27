ba39db9eacb778dcfd8145a33accd70d60e3f9dd
Stephen Whitmore <sww@eight45.net>
2017-11-27 15:49:00 -0800

    test: add another changeset index test

--- test/changesets.js
+++ test/changesets.js
@@ -49,3 +49,56 @@ test('changeset: get elements', function (t) {
   })
 })
 
+test('changeset: multiple changesets', function (t) {
+  var db = createDb()
+
+  var node = {
+    type: 'node',
+    changeset: '9',
+    lat: '-11',
+    lon: '-10',
+    timestamp: '2017-10-10T19:55:08.570Z'
+  }
+
+  var way = {
+    type: 'way',
+    changeset: '9',
+    refs: ['bob', 'dole', 'for', 'prez']
+  }
+
+  var relation = {
+    type: 'relation',
+    changeset: '21',
+    tags: {
+      waterway: 'river'
+    },
+    members: [
+      {
+        type: 'node',
+        id: '101'
+      }
+    ]
+  }
+
+  db.osm.create(node, function (err, id1) {
+    t.ifError(err)
+    db.osm.create(way, function (err, id2) {
+      t.ifError(err)
+      db.osm.create(relation, function (err, id3) {
+        t.ifError(err)
+        db.osm.getChanges('9', function (err, res) {
+          var expected = [id1, id2].sort()
+          t.equals(res.length, 2)
+          t.deepEquals(res, expected)
+          db.osm.getChanges('21', function (err, res) {
+            var expected = [id3]
+            t.equals(res.length, 1)
+            t.deepEquals(res, expected)
+            t.end()
+          })
+        })
+      })
+    })
+  })
+})
+
