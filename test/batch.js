9e94056358cede9340e69282e7fbfaf0443cae65
Stephen Whitmore <sww@eight45.net>
2017-12-06 18:25:25 -0800

    test: batch tests

---
+++ test/batch.js
@@ -0,0 +1,83 @@
+var test = require('tape')
+var createDb = require('./lib/create-db')
+
+test('create nodes', function (t) {
+  var db = createDb()
+
+  t.plan(1)
+
+  var nodes = [
+    {
+      type: 'node',
+      changeset: '9',
+      lat: '-11',
+      lon: '-10',
+      timestamp: '2017-10-10T19:55:08.570Z'
+    },
+    {
+      type: 'node',
+      changeset: '9',
+      lat: '-11',
+      lon: '-10'
+    }
+  ]
+
+  var batch = nodes.map(function (node) {
+    return {
+      type: 'put',
+      value: node
+    }
+  })
+
+  db.osm.batch(batch, function (err) {
+    t.error(err)
+    // TODO: do a hyperdb#createHistoryStream to check for entries
+  })
+})
+
+test('create + update nodes', function (t) {
+  var db = createDb()
+
+  t.plan(5)
+
+  var nodes = [
+    {
+      type: 'node',
+      changeset: '9',
+      lat: '-11',
+      lon: '-10',
+      timestamp: '2017-10-10T19:55:08.570Z'
+    },
+    {
+      type: 'node',
+      changeset: '9',
+      lat: '-11',
+      lon: '-10'
+    }
+  ]
+
+  db.osm.create(nodes[0], function (err, id) {
+    t.error(err)
+
+    nodes[0].lat = '75'
+
+    var batch = nodes.map(function (node, idx) {
+      return {
+        type: 'put',
+        id: idx === 0 ? id : undefined,
+        value: node
+      }
+    })
+
+    db.osm.batch(batch, function (err) {
+      t.error(err)
+      db.osm.get(id, function (err, elements) {
+        t.equals(elements.length, 1)
+        var element = elements[0]
+        t.equals(element.id, id)
+        t.equals(element.lat, '75')
+      })
+      // TODO: do a hyperdb#createHistoryStream to check for entries
+    })
+  })
+})
