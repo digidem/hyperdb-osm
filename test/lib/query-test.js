980c95542b075ac1d5065ed93f3d0b71c4994684
Stephen Whitmore <sww@eight45.net>
2018-01-13 16:49:50 -0500

    test: query test helper module

---
+++ test/lib/query-test.js
@@ -0,0 +1,42 @@
+module.exports = function (t, db, data, expected, cb) {
+  var batch = data.map(function (elm) {
+    var id = elm.id
+    delete elm.id
+    return {
+      type: 'put',
+      id: id,
+      value: elm
+    }
+  })
+
+  db.osm.batch(batch, function (err) {
+    t.error(err)
+
+    ;(function next () {
+      var q = expected.shift()
+      if (!q) return cb()
+      var pending = 2
+
+      db.osm.query(q.bbox, function (err, res) {
+        t.error(err, 'no error on cb query')
+        var ids = res.map(function (elm) { return elm.id }).sort()
+        t.deepEquals(ids, q.expected.sort(), 'ids match cb query')
+        if (!--pending) next()
+      })
+
+      collect(db.osm.query(q.bbox), function (err, res) {
+        t.error(err, 'no error on streaming query')
+        var ids = res.map(function (elm) { return elm.id }).sort()
+        t.deepEquals(ids, q.expected.sort(), 'ids match cb query')
+        if (!--pending) next()
+      })
+    })()
+  })
+}
+
+function collect (stream, cb) {
+  var res = []
+  stream.on('data', res.push.bind(res))
+  stream.once('end', cb.bind(null, null, res))
+  stream.once('error', cb.bind(null))
+}
