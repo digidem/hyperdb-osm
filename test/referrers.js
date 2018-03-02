9cdaf93d100c9a224e03ebfa19883af5851c0084
Stephen Whitmore <sww@eight45.net>
2018-03-02 10:37:54 -0800

    fix: remove previous heads from the refs index

--- test/referrers.js
+++ test/referrers.js
@@ -70,3 +70,88 @@ test('getReferrers API', function (t) {
     })
   })
 })
+
+test('return only latest referrers to a node: way', function (t) {
+  var db = createDb()
+
+  var data = [
+    { type: 'node',
+      id: 'A',
+      lat: '0',
+      lon: '0',
+      tags: {} },
+    { type: 'node',
+      id: 'B',
+      lat: '1',
+      lon: '1',
+      tags: {} },
+    { type: 'node',
+      id: 'C',
+      lat: '2',
+      lon: '2',
+      tags: {} },
+    { type: 'way',
+      id: 'D',
+      refs: ['A', 'B', 'C'],
+      tags: {} }
+  ]
+
+  setup(db, data, function (err) {
+    t.error(err)
+
+    // Update way
+    var way = data[3]
+    way.tags = { foo: 'bar' }
+    way.changeset = '123'
+
+    db.put('D', way, function (err) {
+      t.error(err)
+      db.getReferrers('A', function (err, refs) {
+        t.error(err)
+        t.equals(refs.length, 1)
+        t.equals(refs[0].id, 'D')
+        t.end()
+      })
+    })
+  })
+})
+
+test('return only latest referrers to a node: relation', function (t) {
+  var db = createDb()
+
+  var data = [
+    { type: 'node',
+      id: 'A',
+      lat: '0',
+      lon: '0',
+      tags: {} },
+    { type: 'node',
+      id: 'B',
+      lat: '1',
+      lon: '1',
+      tags: {} },
+    { type: 'relation',
+      id: 'C',
+      members: [ { type: 'node', id: 'B' } ],
+      tags: {} }
+  ]
+
+  setup(db, data, function (err) {
+    t.error(err)
+
+    // Update relation
+    var rel = data[2]
+    rel.tags = { foo: 'bar' }
+    rel.changeset = '123'
+    rel.members = []
+
+    db.put('C', rel, function (err) {
+      t.error(err)
+      db.getReferrers('B', function (err, refs) {
+        t.error(err)
+        t.equals(refs.length, 0)
+        t.end()
+      })
+    })
+  })
+})
